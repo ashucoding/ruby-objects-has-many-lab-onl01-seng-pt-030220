3f71adddb0afd10f555f41c375c6268111b78b9e
Shana Wiltshire <shanawiltshire@gmail.com>
2020-04-25 20:46:34 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+attr_accessor :name, :songs
+
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+    @@song_count += 1
+     @songs << song
+  end
+
+def song_count
+  self.songs.size 
+end
+
+  def self.song_count
+     @songs << song
+  end
+
+end
